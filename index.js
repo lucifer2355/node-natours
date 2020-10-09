f4dc1da8c2c31bbea6af058eb051eec5f86cee17
lucifer2355 <dgajjar999@gmail.com>
2020-10-09 23:58:45 +0530

    remove nide modules

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Node Project");
+  res.send("Hello Node Projects");
 });
 
 const port = 3000;
