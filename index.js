4876d9bdc212880065f03937cf9d5221fd30f0f7
lucifer2355 <dgajjar999@gmail.com>
2020-10-10 21:55:38 +0530

    add post method & get tour by id

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const { fail } = require("assert");
 const express = require("express");
 const fs = require("fs");
+
 const app = express();
+app.use(express.json());
 
 // app.get("/", (req, res) => {
 //   res.status(200).json({ message: "Hello Node Projects", app: "natours" });
@@ -18,6 +21,41 @@ app.get("/api/tours", (req, res) => {
   res
     .status(200)
     .json({ status: "success", results: tours.length, data: { tours } });
+});
+
+app.get("/api/tours/:id", (req, res) => {
+  const id = req.params.id + 1;
+  if (id > tours.length) {
+    return res.status(404).json({ status: "fail", message: "Invalid ID" });
+  }
+
+  const tour = tours.find((el) => el.id === id);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post("/api/tours", (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "sucess",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
