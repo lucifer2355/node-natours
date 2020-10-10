1e4c2d17ffdf54f4fca5f77212c87b74c95fba29
lucifer2355 <dgajjar999@gmail.com>
2020-10-10 11:27:39 +0530

    add get tours api

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require("express");
-
+const fs = require("fs");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello Node Projects", app: "natours" });
-});
+// app.get("/", (req, res) => {
+//   res.status(200).json({ message: "Hello Node Projects", app: "natours" });
+// });
 
-app.post("/", (req, res) => {
-  res.send("you can post to this endpoint");
+// app.post("/", (req, res) => {
+//   res.send("you can post to this endpoint");
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/tours", (req, res) => {
+  res
+    .status(200)
+    .json({ status: "success", results: tours.length, data: { tours } });
 });
 
 const port = 3000;
