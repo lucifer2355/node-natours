8daee17e51595e64131a38c1d82b7a4908844365
lucifer2355 <dgajjar999@gmail.com>
2020-10-11 16:36:49 +0530

    add user routes

--- index.js
+++ index.js
@@ -102,9 +102,47 @@ const deleteTour = (req, res) => {
   });
 };
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined!",
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined!",
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined!",
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined!",
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined!",
+  });
+};
+
 //!ROUTES
 app.route("/api/tours").get(getAllTours).post(createTour);
 app.route("/api/tours/:id").get(getTour).patch(updateTour).delete(deleteTour);
+
+app.route("/api/users").get(getAllUsers).post(createUser);
+app.route("/api/users/:id").get(getUser).patch(updateUser).delete(deleteUser);
 
 //! START SERVER
 const port = 3000;
