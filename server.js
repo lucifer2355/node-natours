8bce861bd96feaed71f61abf94835f084e1bc152
lucifer2355 <dgajjar999@gmail.com>
2020-10-16 22:19:02 +0530

    connect to database

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = require("./index");
 
 dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log(con.connection);
+    console.log("DB connecion succesfull");
+  });
 
 app.listen(process.env.PORT, () => {
   console.log(`app running on port ${process.env.PORT}...`);
