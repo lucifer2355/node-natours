fcbbec0c301c0669b406a9518059010d44ea8073
lucifer2355 <dgajjar999@gmail.com>
2020-10-16 22:31:46 +0530

    add schema & model

--- server.js
+++ server.js
@@ -16,10 +16,24 @@ mongoose
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
-  .then((con) => {
-    console.log(con.connection);
+  .then(() => {
     console.log("DB connecion succesfull");
   });
+
+const toursSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"],
+    unique: true,
+  },
+  price: { type: Number, required: [true, "A tour must have a price"] },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+});
+
+const Tours = mongoose.model("Tours", toursSchema);
 
 app.listen(process.env.PORT, () => {
   console.log(`app running on port ${process.env.PORT}...`);
