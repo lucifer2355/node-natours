490ad1dbddc48a79d6b67e1311408409abd0c7f0
lucifer2355 <dgajjar999@gmail.com>
2020-10-17 00:38:32 +0530

    add mvc structure

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,41 +1,26 @@
-const fs = require("fs");
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkID = (req, res, next, val) => {
-  console.log(`id: ${val}`);
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    });
-  }
-  next();
-};
+const Tour = require("../models/tourModel");
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
     requestTime: req.requestTime,
-    results: tours.length,
-    data: {
-      tours,
-    },
+    // results: tours.length,
+    // data: {
+    //   tours,
+    // },
   });
 };
 
 exports.getTour = (req, res) => {
   const id = req.params.id * 1;
 
-  const tour = tours.find((el) => el.id === id);
+  // const tour = tours.find((el) => el.id === id);
 
   res.status(200).json({
     status: "success",
-    data: {
-      tour,
-    },
+    // data: {
+    //   tour,
+    // },
   });
 };
 
--- server.js
+++ server.js
@@ -20,31 +20,16 @@ mongoose
     console.log("DB connecion succesfull");
   });
 
-const toursSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "A tour must have a name"],
-    unique: true,
-  },
-  price: { type: Number, required: [true, "A tour must have a price"] },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-});
-
-const Tours = mongoose.model("Tours", toursSchema);
-
-const testTour = new Tours({
-  name: "The Park Camper",
-  price: 997,
-});
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => console.log("Error in doc", err));
+// const testTour = new Tours({
+//   name: "The Park Camper",
+//   price: 997,
+// });
+// testTour
+//   .save()
+//   .then((doc) => {
+//     console.log(doc);
+//   })
+//   .catch((err) => console.log("Error in doc", err));
 
 app.listen(process.env.PORT, () => {
   console.log(`app running on port ${process.env.PORT}...`);
