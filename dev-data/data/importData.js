cbf811edf0acc030e6a2b00cfb7eb525775d9fd1
lucifer2355 <dgajjar999@gmail.com>
2020-10-17 19:01:25 +0530

    add import & delete data function

---
+++ dev-data/data/importData.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const fs = require("fs");
+const Tour = require("../../models/tourModel");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB connecion succesfull");
+  });
+
+//! Read JSON file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, "utf-8")
+);
+
+//! Import data into database
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log("Data  successfully loaded!");
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//! Delete all data form database
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log("Data successfully deleted!");
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+if (process.argv[2] === "--import") {
+  importData();
+}
+if (process.argv[2] === "--delete") {
+  deleteData();
+}
