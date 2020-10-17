10242ebf91626bf166b0ae7af470993e350314f8
lucifer2355 <dgajjar999@gmail.com>
2020-10-18 00:38:42 +0530

    add sorting query

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,12 +4,20 @@ exports.getAllTours = async (req, res) => {
   try {
     const queryObj = { ...req.query };
     const excludedFields = ["page", "sort", "limit", "fields"];
-    excludedFields.forEach((el) => delete queryObj(el));
+    excludedFields.forEach((el) => delete queryObj[el]);
 
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b{gte|gt|lte|lt}\b/g, (match) => `$${match}`);
 
-    const query = Tour.find(queryStr);
+    let query = Tour.find(JSON.parse(queryStr));
+
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
     const tours = await query;
 
     res.status(200).json({
