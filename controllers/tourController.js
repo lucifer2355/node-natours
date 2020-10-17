d20576b177bdb7318c87f0e512fafdb2e80edb5e
lucifer2355 <dgajjar999@gmail.com>
2020-10-17 11:31:21 +0530

    add get all tours & get one tour from the database

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,27 +1,41 @@
 const Tour = require("../models/tourModel");
 
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    requestTime: req.requestTime,
-    // results: tours.length,
-    // data: {
-    //   tours,
-    // },
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status: "success",
+      requestTime: req.requestTime,
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error,
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
 
-  // const tour = tours.find((el) => el.id === id);
-
-  res.status(200).json({
-    status: "success",
-    // data: {
-    //   tour,
-    // },
-  });
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error,
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
