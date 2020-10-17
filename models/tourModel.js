48c83d54e076d1ec2ed61e9df90770761b8b9ba7
lucifer2355 <dgajjar999@gmail.com>
2020-10-17 14:10:10 +0530

    add more information in model

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,11 +5,52 @@ const toursSchema = new mongoose.Schema({
     type: String,
     required: [true, "A tour must have a name"],
     unique: true,
+    trim: true,
+  },
+  duration: {
+    type: Number,
+    required: [true, "A tour must have a duration"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "A tour must have a group size"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, "A tour must have a difficulty"],
   },
   price: { type: Number, required: [true, "A tour must have a price"] },
-  rating: {
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, "A tour must have a summary"],
+  },
+  ratingAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, "A tour must have  image cover"],
+  },
+  images: {
+    type: [String],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: {
+    type: [Date],
   },
 });
 
