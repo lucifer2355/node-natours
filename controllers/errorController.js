a6dbfd866f218c5926b926e19c15d191cc3ab5ec
lucifer2355 <dgajjar999@gmail.com>
2020-10-25 15:20:29 +0530

    add duplicate field error handler function

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,13 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 404);
 };
 
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+
+  const message = `Duplocate field value: ${value}, Please use another value!`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -45,6 +52,8 @@ module.exports = (err, req, res, next) => {
     let error = { ...err };
 
     if (error.name === "CastError") error = handleCastErrorDB(error);
+    if (error.code === 11000) (error) => handleDuplicateFieldsDB(error);
+
     sendErrorProd(error, res);
   }
 };
