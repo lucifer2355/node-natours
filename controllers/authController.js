916a01ecd0c544e7fb6e8df49e33a07898f4a97e
lucifer2355 <dgajjar999@gmail.com>
2020-10-27 22:29:20 +0530

    create protect middleware & get token from the headers

--- controllers/authController.js
+++ controllers/authController.js
@@ -49,3 +49,26 @@ exports.login = catchAsync(async (req, res, next) => {
     token,
   });
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+  //! Getting token and check of it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  if (!token) {
+    return next(
+      new AppError("You are not logged in! Please log in to get access", 401)
+    );
+  }
+  //! Verification token
+
+  //! Check if user is still exists
+
+  //! Check if user chnaged password after the token was issue
+  next();
+});
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { protect } = require("../controllers/authController");
 const {
   getAllTours,
   createTour,
@@ -15,7 +16,7 @@ const router = express.Router();
 router.route("/top-5-cheap").get(aliasTopTours, getAllTours);
 router.route("/tour-stats").get(getTourStats);
 router.route("/monthly-plan/:year").get(getMonthlyPlan);
-router.route("/").get(getAllTours).post(createTour);
+router.route("/").get(protect, getAllTours).post(createTour);
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
