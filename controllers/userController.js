dd39fa591e9c94fc81e2436d0f607db31109a831
lucifer2355 <dgajjar999@gmail.com>
2020-10-30 22:34:11 +0530

    add update me function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,14 @@
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
@@ -12,6 +21,33 @@ exports.getAllUsers = catchAsync(async (req, res) => {
     },
   });
 });
+
+exports.updateMe = async (req, res, next) => {
+  //! Create error if user POSts password data
+  if (req.body.password || req.body.passwordConfirm)
+    return next(
+      new AppError(
+        "This route is not for password updates. Please use updateMyPassword",
+        400
+      )
+    );
+
+  //! Filter req body fields
+  const filteredBody = filterObj(req.body, "name", "email");
+
+  //! Update user data
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      user: updatedUser,
+    },
+  });
+};
 
 exports.getUser = (req, res) => {
   res.status(500).json({
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,7 @@ const {
   getUser,
   updateUser,
   deleteUser,
+  updateMe,
 } = require("../controllers/userController");
 const {
   signup,
@@ -22,6 +23,7 @@ router.post("/login", login);
 router.post("/forgotPassword", forgotPassword);
 router.patch("/resetPassword/:token", resetPassword);
 router.patch("/updateMyPassword", protect, updatePassword);
+router.patch("/updateMe", protect, updateMe);
 router.route("/").get(getAllUsers).post(createUser);
 router.route("/").get(getUser).patch(updateUser).delete(deleteUser);
 
