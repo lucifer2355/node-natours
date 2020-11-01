f9fcc7410d5951f94d1378d4f9a1831ed8e5a231
lucifer2355 <dgajjar999@gmail.com>
2020-11-01 23:50:13 +0530

    add mongo sanitize & xss clean for no SQL query injection

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 const AppError = require("./utils/appError");
 const globErrorHandler = require("./controllers/errorController");
@@ -32,6 +34,12 @@ app.use(express.json({ limit: "10kb" }));
 
 //* Serving static file
 app.use(express.static(`${__dirname}/public`));
+
+//* Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+//* Data sanitization against XSS
+app.use(xss());
 
 //* Test middleware
 app.use((req, res, next) => {
