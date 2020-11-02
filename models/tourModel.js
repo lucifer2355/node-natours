a288ff481bffef8bbb1c41197df3f4a57b2f4b43
lucifer2355 <dgajjar999@gmail.com>
2020-11-02 23:48:53 +0530

    add location model

--- models/tourModel.js
+++ models/tourModel.js
@@ -78,6 +78,26 @@ const toursSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    startLocation: {
+      //! GeoJSON
+      type: {
+        type: String,
+        default: "Point",
+        enum: ["Point"],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: { type: String, default: "Point", enum: ["Point"] },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
