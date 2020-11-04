433acb34c3d7e91eb1ab22f4bbdeece0e43e2275
“lucifer2355” <dgajjar999@gmail.com>
2020-11-05 00:29:14 +0530

    create review model

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
 const validator = require("validator");
-const User = require("./userModel");
+// const User = require("./userModel");
 
 const toursSchema = new mongoose.Schema(
   {
