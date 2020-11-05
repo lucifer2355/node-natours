4acd7eed47120f53c008631a90c45de845dd7cc8
“lucifer2355” <dgajjar999@gmail.com>
2020-11-06 00:00:09 +0530

    add some auth condition in create review route

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -3,9 +3,13 @@ const {
   getAllReviews,
   createReview,
 } = require("../controllers/reviewController");
+const { protect, restrictTo } = require("../controllers/authController");
 
 const router = express.Router();
 
-router.route("/").get(getAllReviews).post(createReview);
+router
+  .route("/")
+  .get(getAllReviews)
+  .post(protect, restrictTo("user"), createReview);
 
 module.exports = router;
