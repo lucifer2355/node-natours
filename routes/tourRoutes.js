f41becdae73acaf51bce245805f3b7386ba705d8
“lucifer2355” <dgajjar999@gmail.com>
2020-11-07 00:11:12 +0530

    add nested router using express

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
+const reviewRouter = require("./reviewRoutes");
 const { protect, restrictTo } = require("../controllers/authController");
-const { createReview } = require("../controllers/reviewController");
 const {
   getAllTours,
   createTour,
@@ -14,6 +14,8 @@ const {
 
 const router = express.Router();
 
+router.use("/:tourId/reviews", reviewRouter);
+
 router.route("/top-5-cheap").get(aliasTopTours, getAllTours);
 router.route("/tour-stats").get(getTourStats);
 router.route("/monthly-plan/:year").get(getMonthlyPlan);
@@ -23,12 +25,5 @@ router
   .get(getTour)
   .patch(updateTour)
   .delete(protect, restrictTo("admin", "lead-guide"), deleteTour);
-
-//* POST /tour/(tourID)/reviews
-//* GET /tour/(tourID)/reviews
-//* GET /tour/(tourId)/reviews/(reviewId)
-router
-  .route("/:tourId/reviews")
-  .post(protect, restrictTo("user"), createReview);
 
 module.exports = router;
