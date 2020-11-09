22ee176031812885cae4d298eb74e2ce3b909933
“lucifer2355” <dgajjar999@gmail.com>
2020-11-10 00:17:51 +0530

    add handle factory function for delete modal data

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {
   getAllReviews,
   createReview,
+  deleteReview,
 } = require("../controllers/reviewController");
 const { protect, restrictTo } = require("../controllers/authController");
 
@@ -11,5 +12,6 @@ router
   .route("/")
   .get(getAllReviews)
   .post(protect, restrictTo("user"), createReview);
+router.route("/:id").delete(deleteReview);
 
 module.exports = router;
